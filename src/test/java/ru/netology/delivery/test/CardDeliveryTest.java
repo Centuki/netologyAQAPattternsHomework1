218eabc0fb642834f73c81216fdb082353dbaee4
Ruslan Somov <rusl.somow@gmail.com>
2026-03-27 12:09:37 +0300

    allure added

--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -1,10 +1,9 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Selectors;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -16,6 +15,12 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardDeliveryTest {
+
+    @BeforeAll
+    static void setupAll() {SelenideLogger.addListener("allure", new AllureSelenide());}
+
+    @AfterAll
+    static void tearDownAll() {SelenideLogger.removeListener("allure");}
 
     @BeforeEach
     void setup(){open("http://localhost:9999");}
